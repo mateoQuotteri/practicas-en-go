c5f19aaa518b66a217c37866d949c5b558efde09
Mateo <quotterimateo@gmail.com>
2025-04-04 19:33:36 -0300

    simplezas en go

--- variables/main.go
+++ variables/main.go
@@ -8,4 +8,8 @@ func main() {
 	var myIntVar int
 	myIntVar = -12
 	fmt.Println("Enter an integer:", myIntVar)
+
+	myString := "Hello, World!"
+	fmt.Println("Enter a string:", myString)
+	fmt.Println("myString espacio en memoria:", &myString)
 }
