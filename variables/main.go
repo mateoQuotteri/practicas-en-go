f65d1527259a5ba28dead7d2ec268f26680ea1b7
Mateo <quotterimateo@gmail.com>
2025-04-05 14:49:37 -0300

    jugando con constantes y scopes

--- variables/main.go
+++ variables/main.go
@@ -12,4 +12,13 @@ func main() {
 	myString := "Hello, World!"
 	fmt.Println("Enter a string:", myString)
 	fmt.Println("myString espacio en memoria:", &myString)
+
+	{
+		const myConstVar = 3.14 // Constantes no pueden ser cambiadas
+		fmt.Println("mi constante:", myConstVar)
+
+		// myConstVar = 2.71 // Esto generará un error de compilación
+
+	}
+
 }
