60bd2b4eaa309629ba91a862e591c4a56acf7e90
Mateo <quotterimateo@gmail.com>
2025-04-11 17:34:24 -0300

    generics 2

--- generics/main.go
+++ generics/main.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func main() {
 
-	v1 := []float64{1.0, 2.0, 3.0}
-	v2 := []int32{1, 2, 3}
+	//v1 := []float64{1.0, 2.0, 3.0}
+	// v2 := []int32{1, 2, 3}
 
-	fmt.Println("La suma de los valores de v1 es:", Sum01(v1))
+	/*fmt.Println("La suma de los valores de v1 es:", Sum01(v1))
 	fmt.Println("La suma de los valores de v2 es:", Sum01(v2))
 
 	fmt.Println("La suma de los valores de v1 es:", Sum02(v1))
 	fmt.Println("La suma de los valores de v2 es:", Sum02(v2))
+	*/
+	AnyTipe(1, 8)
+	AnyTipe2("Hola", 8)
 
 }
 
@@ -37,4 +40,18 @@ func Sum02[N Number](v []N) N {
 		total += v
 	}
 	return total
+}
+
+//Recibe CUAKQUIER TIPO y devuelve CUAKQUIER TIPO
+// LOS DOS VALORES PASADOS POR PARAMRETROS DEBEN SER DEL MMISMO
+//TIPO
+func AnyTipe[N any](v1, v2 N) {
+	fmt.Println("Valores:", v1, v2)
+}
+
+//Recibe CUAKQUIER TIPO y devuelve CUAKQUIER TIPO
+// las dos variables pueden ser de diferente tipo
+// y no tienen que ser del mismo tipo
+func AnyTipe2[N1 any, N2 any](v1 N1, v2 N2) {
+	fmt.Println("Valores:", v1, v2)
 }
